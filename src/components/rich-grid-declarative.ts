9e841d5678b249dbb53d1e2aef12d03d136a432b
Sean Landsman <sean@thelandsmans.com>
2016-11-17 10:51:21 +0000

    Prepare for 7.x, add declaritive rich grid example

--- src/components/rich-grid-declarative.ts
+++ src/components/rich-grid-declarative.ts
@@ -1,20 +1,20 @@
 import {autoinject, customElement} from 'aurelia-framework'
+
 import RefData from '../data/refData';
+import SkillFilter from '../renderers/skillFilter';
+import ProficiencyFilter from '../renderers/proficiencyFilter';
 
 import {GridOptions, GridApi, ColumnApi} from 'ag-grid';
 // only import this if you are using the ag-Grid-Enterprise
 import 'ag-grid-enterprise/main';
-import SkillFilter from "../renderers/skillFilter";
-import ProficiencyFilter from "../renderers/proficiencyFilter";
 
 @autoinject()
-@customElement('rich-grid')
-export class RichGrid {
+@customElement('rich-grid-declarative')
+export class RichGridDeclarative {
 
   private gridOptions: GridOptions;
   private showGrid: boolean;
   private rowData: any[];
-  private columnDefs: any[];
   private rowCount: string;
   private api: GridApi;
   private columnApi: ColumnApi;
@@ -23,7 +23,6 @@ export class RichGrid {
     // we pass an empty gridOptions in, so we can grab the api out
     this.gridOptions = <GridOptions>{};
     this.createRowData();
-    this.createColumnDefs();
     this.showGrid = true;
 
     this.gridOptions.onGridReady = () => {
@@ -53,62 +52,12 @@ export class RichGrid {
         country: countryData.country,
         continent: countryData.continent,
         language: countryData.language,
-        mobile: createRandomPhoneNumber(),
-        landline: createRandomPhoneNumber()
+        mobile: this.createRandomPhoneNumber(),
+        landline: this.createRandomPhoneNumber()
       });
     }
 
     this.rowData = rowData;
-  }
-
-  private createColumnDefs() {
-    this.columnDefs = [
-      {
-        headerName: '#', width: 30, checkboxSelection: true, suppressSorting: true,
-        suppressMenu: true, pinned: true
-      },
-      {
-        headerName: 'Employee',
-        children: [
-          {
-            headerName: "Name", field: "name",
-            width: 150, pinned: true
-          },
-          {
-            headerName: "Country", field: "country", width: 150,
-            cellRenderer: countryCellRenderer, pinned: true,
-            filterParams: {cellRenderer: countryCellRenderer, cellHeight: 20}
-          },
-        ]
-      },
-      {
-        headerName: 'IT Skills',
-        children: [
-          {
-            headerName: "Skills",
-            width: 125,
-            suppressSorting: true,
-            cellRenderer: skillsCellRenderer,
-            filter: SkillFilter
-          },
-          {
-            headerName: "Proficiency",
-            field: "proficiency",
-            width: 120,
-            cellRenderer: percentCellRenderer,
-            filter: ProficiencyFilter
-          },
-        ]
-      },
-      {
-        headerName: 'Contact',
-        children: [
-          {headerName: "Mobile", field: "mobile", width: 150, filter: 'text'},
-          {headerName: "Land-line", field: "landline", width: 150, filter: 'text'},
-          {headerName: "Address", field: "address", width: 500, filter: 'text'}
-        ]
-      }
-    ];
   }
 
   private calculateRowCount() {
@@ -199,57 +148,73 @@ export class RichGrid {
     console.log('onColumnEvent: ' + $event);
   }
 
-}
 
-function skillsCellRenderer(params) {
-  var data = params.data;
-  var skills = [];
-  RefData.IT_SKILLS.forEach(function (skill) {
-    if (data && data.skills && data.skills[skill]) {
-      skills.push('<img src="/images/skills/' + skill + '.png" width="16px" title="' + skill + '" />');
-    }
-  });
-  return skills.join(' ');
-}
-
-function countryCellRenderer(params) {
-  var flag = "<img border='0' width='15' height='10' style='margin-bottom: 2px' src='../images/flags/" + RefData.COUNTRY_CODES[params.value] + ".png'>";
-  return flag + " " + params.value;
-}
-
-function createRandomPhoneNumber() {
-  var result = '+';
-  for (var i = 0; i < 12; i++) {
-    result += Math.round(Math.random() * 10);
-    if (i === 2 || i === 5 || i === 8) {
-      result += ' ';
-    }
-  }
-  return result;
-}
-
-function percentCellRenderer(params) {
-  var value = params.value;
-
-  var eDivPercentBar = document.createElement('div');
-  eDivPercentBar.className = 'div-percent-bar';
-  eDivPercentBar.style.width = value + '%';
-  if (value < 20) {
-    eDivPercentBar.style.backgroundColor = 'red';
-  } else if (value < 60) {
-    eDivPercentBar.style.backgroundColor = '#ff9900';
-  } else {
-    eDivPercentBar.style.backgroundColor = '#00A000';
+  private countryCellRenderer(params) {
+    var flag = "<img border='0' width='15' height='10' style='margin-bottom: 2px' src='../images/flags/" + RefData.COUNTRY_CODES[params.value] + ".png'>";
+    return flag + " " + params.value;
   }
 
-  var eValue = document.createElement('div');
-  eValue.className = 'div-percent-value';
-  eValue.innerHTML = value + '%';
+  private skillsCellRenderer(params) {
+    var data = params.data;
+    var skills = [];
+    RefData.IT_SKILLS.forEach(function (skill) {
+      if (data && data.skills && data.skills[skill]) {
+        skills.push('<img src="/images/skills/' + skill + '.png" width="16px" title="' + skill + '" />');
+      }
+    });
+    return skills.join(' ');
+  }
 
-  var eOuterDiv = document.createElement('div');
-  eOuterDiv.className = 'div-outer-div';
-  eOuterDiv.appendChild(eValue);
-  eOuterDiv.appendChild(eDivPercentBar);
+  private percentCellRenderer(params) {
+    var value = params.value;
 
-  return eOuterDiv;
+    var eDivPercentBar = document.createElement('div');
+    eDivPercentBar.className = 'div-percent-bar';
+    eDivPercentBar.style.width = value + '%';
+    if (value < 20) {
+      eDivPercentBar.style.backgroundColor = 'red';
+    } else if (value < 60) {
+      eDivPercentBar.style.backgroundColor = '#ff9900';
+    } else {
+      eDivPercentBar.style.backgroundColor = '#00A000';
+    }
+
+    var eValue = document.createElement('div');
+    eValue.className = 'div-percent-value';
+    eValue.innerHTML = value + '%';
+
+    var eOuterDiv = document.createElement('div');
+    eOuterDiv.className = 'div-outer-div';
+    eOuterDiv.appendChild(eValue);
+    eOuterDiv.appendChild(eDivPercentBar);
+
+    return eOuterDiv;
+  }
+
+  private getSkillFilter(): any {
+    return SkillFilter;
+  }
+
+  private getProficiencyFilter(): any {
+    return ProficiencyFilter;
+  }
+
+  private getCountryFilterParams(): any {
+    return {
+      cellRenderer: this.countryCellRenderer,
+      cellHeight: 20
+    }
+  }
+
+  private createRandomPhoneNumber() {
+    var result = '+';
+    for (var i = 0; i < 12; i++) {
+      result += Math.round(Math.random() * 10);
+      if (i === 2 || i === 5 || i === 8) {
+        result += ' ';
+      }
+    }
+    return result;
+  }
 }
+

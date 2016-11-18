8aa2f96a6edcb30f02cf4c446969926159bba93e
Sean Landsman <sean@thelandsmans.com>
2016-11-18 21:26:00 +0000

    full width example

---
+++ src/renderers/NameAndAgeRenderer.ts
@@ -0,0 +1,19 @@
+import {ICellRenderer} from "ag-grid/main";
+
+export default class NameAndAgeRenderer implements ICellRenderer {
+  params:any;
+
+  eGui:HTMLElement;
+  init(params: any): void {
+    this.params = params;
+
+    this.eGui = document.createElement('span');
+
+    let text = document.createTextNode(`Name: ${params.data.name}, Age: ${params.data.age}`);
+    this.eGui.appendChild(text);
+  }
+
+  getGui(): HTMLElement {
+    return this.eGui;
+  }
+}

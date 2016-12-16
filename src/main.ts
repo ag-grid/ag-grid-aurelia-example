af4e9391d4a3c3e38f2b6293f5dca5bbdbf25093
Sean Landsman <sean@thelandsmans.com>
2016-12-16 09:13:58 +0000

    Add license key example for users

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
 
+// enterprise users uncomment this
+// import { LicenseManager } from 'ag-grid-enterprise/main';
+
 //Configure Bluebird Promises.
 //Note: You may want to use environment-specific configuration.
 (<any>Promise).config({
@@ -15,9 +18,13 @@ export function configure(aurelia: Aurelia) {
     .plugin('ag-grid-aurelia')
     .feature('resources');
 
+  // enterprise uncomment and set licence key here
+  // LicenseManager.setLicenseKey('LICENSE KEY');
+
   if (environment.debug) {
     aurelia.use.developmentLogging();
   }
+
 
   if (environment.testing) {
     aurelia.use.plugin('aurelia-testing');

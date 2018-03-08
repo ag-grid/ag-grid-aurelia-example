c8ff743c2ded756b552b6dc1244a51786ce55564
Sean Landsman <sean@thelandsmans.com>
2018-03-08 11:58:34 +0000

    AG-1620 Post v17 Tidy/Issue

--- src/filters/skillFilter.ts
+++ src/filters/skillFilter.ts
@@ -7,7 +7,7 @@ var SKILL_TEMPLATE =
     '    <div style="text-align: center;">SKILL_NAME</div>' +
     '    <div>' +
     '      <input type="checkbox"/>' +
-    '      <img src="/images/skills/SKILL.png" width="30px"/>' +
+    '      <img src="https://raw.githubusercontent.com/ag-grid/ag-grid-docs/master/src/images/skills/SKILL.png" width="30px"/>' +
     '    </div>' +
     '  </span>' +
     '</label>';

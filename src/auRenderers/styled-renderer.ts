f1e211edcdb354c2f0a11e1cfebda20a7ae8ba21
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-24 13:46:44 +0800

    Delete src directory

--- src/auRenderers/styled-renderer.ts
+++
@@ -1,7 +0,0 @@
-import {customElement, inject} from "aurelia-framework";
-
-@customElement('ag-styled-renderer')
-@inject(Element)
-export class StyledRenderer {
-    params: any;
-}
--- src/components/blank/blank.ts
+++
@@ -1,7 +0,0 @@
-import {autoinject} from 'aurelia-framework'
-
-// for deployment to ag-grid.com only
-// can be ignored
-@autoinject()
-export class Blank {
-}
